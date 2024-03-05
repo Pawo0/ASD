008b677da4f96cb9929f81407612c7d86dfc0c97
Pawo0 <czajczykp@gmail.com>
2024-03-05 01:07:44 +0100

    merge sort - first vers

--- offline1/zad1.py
+++ offline1/zad1.py
@@ -2,8 +2,48 @@ from zad1testy import Node, runtests
 
 
 def SortH(p,k):
-    # tu prosze wpisac wlasna implementacje
-    pass
+    def merge_list(a, b):
+        if a is None:
+            return b
+        if b is None:
+            return a
+
+        if a.val <= b.val:
+            result = a
+            result.next = merge_list(a.next, b)
+        else:
+            result = b
+            result.next = merge_list(a, b.next)
+        return result
+
+    def sort_by_split_list(p):
+        if p is None or p.next is None:
+            return p
+        head = p
+        middle = get_middle(p)
+        last = middle
+        first = middle.next
+        last.next = None
+
+        left = sort_by_split_list(head)
+        right = sort_by_split_list(first)
+
+        res = merge_list(left, right)
+        return res
+
+    def get_middle(p):
+        slow = p
+        fast = p
+        while fast.next is not None and fast.next.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+    g = Node()
+    g.next = p
+    res = sort_by_split_list(p)
+    return res
+
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( SortH, all_tests = True )
+runtests(SortH, all_tests=True)

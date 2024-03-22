f23486256e10969e2ae85768dfff128b10f16a6a
Paweł Czajczyk <czajczykp@gmail.com>
2024-03-23 00:55:18 +0100

    O(n+nk) k - number of not dominated points

--- offline3/zad3.py
+++ offline3/zad3.py
@@ -1,9 +1,42 @@
 from zad3testy import runtests
 
 
+def counting_sort_by(tab, coord):
+    n = len(tab)
+    res = [None] * n
+    maxy = 0
+    for x in tab:
+        maxy = max(maxy, x[coord])
+    tab3 = [0] * (maxy + 1)
+    for i in range(n):
+        tab3[tab[i][coord]] += 1
+    for i in range(1, maxy + 1):
+        tab3[i] += tab3[i - 1]
+    for i in range(len(tab)-1, -1, -1):
+        res[tab3[tab[i][coord]] - 1] = tab[i]
+        tab3[tab[i][coord]] -= 1
+    for i in range(n):
+        tab[i] = res[i]
+
+
 def dominance(P):
-    # tu prosze wpisac wlasna implementacje
-    return -1
+    maks_cords = []
+    counting_sort_by(P, 1)
+    counting_sort_by(P, 0)
+    maks_cords.append(P[-1])
+    for i in range(len(P)-2,-1,-1):
+        if P[i][1] >= maks_cords[-1][1]:
+            maks_cords.append(P[i])
+
+    res = 0
+
+    for cord in maks_cords:
+        cnt = 0
+        for el in P:
+            if el[0] < cord[0] and el[1] < cord[1]:
+                cnt += 1
+        res = max(cnt,res)
+    return res
 
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
